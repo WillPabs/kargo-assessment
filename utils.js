75ffc8422097566b65d6916e84bf30c955b5f6bc
William <pabiterowilliam@yahoo.com>
2021-05-03 17:03:32 -0400

    Updated values of Numbers object

---
+++ utils.js
@@ -0,0 +1,28 @@
+export const Numbers = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+};
+
+
+export const prompt_attributes = [
+    {
+        num1: Number,
+        warning: '1st input is not a number',
+    },
+    {
+        num2: Number,
+        warning: '1st input is not a number',
+    },
+    {
+        num3: Number,
+        warning: '1st input is not a number',
+    },
+]
